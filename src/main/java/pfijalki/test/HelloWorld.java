967c52e7a6eeea397e90f7c56d3a90d18607adb0
Piotr <piotrfoo@protonmail.com>
2017-08-24 18:32:23 +0200

    fix

--- src/main/java/pfijalki/test/HelloWorld.java
+++ src/main/java/pfijalki/test/HelloWorld.java
@@ -3,6 +3,6 @@ package pfijalki.test;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        ffSystem.out.printf("hello world");
+        System.out.printf("hello world");
     }
 }
