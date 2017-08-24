b4207d170387f2ff229443fc0febf5ee13a239cb
Piotr <piotrfoo@protonmail.com>
2017-08-24 14:20:06 +0200

    feature: ci scripts for m2 cache

---
+++ ci/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+export ROOT_FOLDER=$( pwd )
+export REPO=repo
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+. ${SCRIPTS_DIR}/localize_m2.sh
+
+mvn clean test
