4103b8bfe3bc833e6099db83f8b13bf5c08040e5
Piotr <piotrfoo@protonmail.com>
2017-08-24 18:28:08 +0200

    bad world

--- src/main/java/pfijalki/test/HelloWorld.java
+++ src/main/java/pfijalki/test/HelloWorld.java
@@ -3,6 +3,6 @@ package pfijalki.test;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.printf("hello world");
+        ffSystem.out.printf("hello world");
     }
 }
